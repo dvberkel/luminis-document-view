a4eb65342d2ca97cbd09569f9a62515d329321fa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-09 13:24:26 +0100

    Wired model and view
    The documentModel and documentView are wired together. A poor mens
    controller is introduced with some jQuery.

--- js/mvc.js
+++ js/mvc.js
@@ -12,10 +12,24 @@
     // library is used
     var converter = new Markdown.Converter();
 
-    // We create a document model. The `markdown` property is set to a
-    // default invitation to select a document.
+    // We create a document model. 
     var DocumentModel = Backbone.Model.extend({
+	// The `markdown` property is set to a default invitation to select a document.
 	defaults: { markdown: "Select a **document** from the tree." },
+	
+	loadDocument: function(documentUrl) {
+	    // The default "document" is reporting an unsuccessfull fetch.
+	    var result = "could **not** load " + documentUrl;
+	    // Make a synchronous ajax call to fetch the document at `documentUrl`.
+	    $.ajax({
+		url: documentUrl,
+		dataType: "text",
+		success: function(data){ result = data; },
+		async: false
+	    });
+	    // Set the document in the `markdown` property.
+	    this.set({markdown: result});	    
+	}
     });
     
     // We create a document view which will render the document model.
@@ -24,6 +38,12 @@
 	    // ensure that the context for the render method is this
 	    // document view.
 	    _.bindAll(this, 'render');
+	    
+	    // Listen for `change:markdown` events from the
+	    // model. Render the view accordingly.
+	    this.model.bind("change:markdown", function(){
+		this.render();
+	    }, this);
 	    
 	    // Render yourself so a user sees the default message.
 	    this.render();
@@ -40,5 +60,16 @@
 	// ... create a model and a corresponding view.
 	var documentModel = new DocumentModel();
 	new DocumentView({el: $("#mvc-port"), model: documentModel});
+	
+	// The tree is a *implicit* controller. Foreach document in the
+	// tree...
+	$("#mvc-selection li").each(function(){
+	    var element = $(this);
+	    // ... bind the click event to `loadDocument` method of
+	    // `documentModel`.
+	    element.click(function(){
+		documentModel.loadDocument(element.attr('href'));
+	    });
+	});
     });
 })(jQuery);
