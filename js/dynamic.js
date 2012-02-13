164bf0c9d97e75767d3e296888c2bd6326fb9dad
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-13 09:35:30 +0100

    Template used
    An underscore template is used to render the view of a single model.

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,1 +1,35 @@
-// Place holder for the dynamic javascript example.
+// This file will load the document tree menu from the server.
+// This is achieved by a preprocessing step in which the `documents` directory is
+// read and reproduced in a json structure.
+
+// We are using a self evaluating function for two reasons. To limit the scope of
+// defined variable and be certain of the variables `$` and `undefined`.
+(function($,undefined){
+    var DocumentTreeView = Backbone.View.extend({
+	// Define a template for one model element.
+	template: _.template("<li href='<%= documentUrl %>'><%= name %></li>"),
+
+	initialize: function(){
+	    // Bind the context of the render method to this view object.
+	    _.bindAll(this, 'render');
+	    
+	    // render yourself
+	    this.render();
+	},
+	
+	// render the view on screen.
+	render: function(){
+	    $(this.el).html(this.template({documentUrl: 'documents/document1.md', name: 'document 1'}));
+	}
+    });
+
+    // After the document is loaded...
+    $(function(){
+	// ... create a document model and corresponding view.
+	var documentModel = new DocumentModel();
+	new DocumentView({el: $("#dynamic-port"), model: documentModel});
+	
+	// Create a document tree view.
+	documentTreeView = new DocumentTreeView({el: $("#dynamic-selection")});
+    });
+})(jQuery)
