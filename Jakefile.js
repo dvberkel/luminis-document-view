826ff1fc1701966a61034346341fc0267b4a28b1
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-15 09:38:57 +0100

    [REFACTOR] Encoding function
    An encoding function is introduced for the encoding of a file for
    the table of contents.

--- Jakefile.js
+++ Jakefile.js
@@ -2,13 +2,17 @@ var fs = require("fs");
 
 desc("Create table of contents for documents directory");
 task("toc", [], function(){
+    var tocEncodingFor = function(file) {
+	return '{"documentUrl": "' + file + '", "name": "' + file + '"}';
+    }
+
     var files = fs.readdirSync("documents");
     stream = fs.createWriteStream("documents/toc.json");
     stream.once("open", function(){
 	stream.write('{"documents":[');
 	for (var index = 0; index < files.length; index++) {
 	    var file = files[index];
-	    stream.write('{"documentUrl": "' + file + '", "name": "' + file + '"}');
+	    stream.write(tocEncodingFor(file));
 	    if (index < files.length - 1) {
 		stream.write(',');
 	    }
