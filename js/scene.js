b232eb3c4014d0adfd9615597e6881e2317ea68b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-22 08:00:55 +0100

    Scene Documentation
    The documentation for the scene is finished.

--- js/scene.js
+++ js/scene.js
@@ -25,38 +25,56 @@
 	// Add it to the scene.
         scene.add( camera );
 	
-	// There are various predefined geometries which can be used for creating a scene.
+	// There are various predefined 
+	// [geometries](http://mrdoob.github.com/three.js/docs/48/api/core/Geometry.html "Documentation on Geometries")
+	// which can be used for creating a scene.
         geometry = new THREE.CubeGeometry( 200, 200, 200 );
+	
+	// Materials determine how geometries are rendered. 
         material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
 	
+	// Combine the material with the geometry...
         mesh = new THREE.Mesh( geometry, material );
+	// ... and at the result to the scene
         scene.add( mesh );
 	
+	// Choose a renderer for the scene. There are various 
+	// [renderers](http://mrdoob.github.com/three.js/ "Github page on Three.js") available
         renderer = new THREE.CanvasRenderer();
+	
+	// Set configure the renderer aperture.
         renderer.setSize( width, height );
 	
+	// Add the apropriate element to the page. 
 	$("#scene-port").append(renderer.domElement);
     }
-
+    
+    // We define a function to animate the scene
     function animate() {
 
         // note: three.js includes requestAnimationFrame shim
         requestAnimationFrame( animate );
+	transform();
         render();
 
     }
     
-    function render() {
-
+    // A function to transform the mesh generated in `init`
+    function transform() {
         mesh.rotation.x += 0.01;
         mesh.rotation.y += 0.02;
-
+    }
+    
+    // Render the scene with the defined camera.
+    function render() {
         renderer.render( scene, camera );
-
     }
 
+    // When the document is ready loading...
     $(function(){
+	// ... initialize and...
 	init();
+	// ... animate the scene.
 	animate();
     });
     
